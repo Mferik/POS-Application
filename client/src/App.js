9a56599022f60ee4df57aaa927f1f760596ab85d
Mferik <mfatiherikk@gmail.com>
2023-06-12 12:49:38 +0300

    Proje Başlangıç Gereksinimleri Yüklendi

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl font-bold underline text-red-800">
+         MFELENDİN
+        </h1>
+      </div>
+      <div>
+        <Button className=" text-yellow-300" type="primary">
+          MFELENDİN
+        </Button>
+      </div>
+      <div>
+        <Button className=" text-yellow-300" type="primary" danger>
+          MFELENDİN
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
