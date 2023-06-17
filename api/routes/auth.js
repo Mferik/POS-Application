623200d444df5285bc08b994dd8e1c1b1f2463e7
Mferik <mfatiherikk@gmail.com>
2023-06-17 13:53:47 +0300

    hashed password güvenli parola oluşturma eklendi

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,19 +1,24 @@
 const User = require("../models/User.js");
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs");
 //! kullanıcı oluşturma
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const { username, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
     await newUser.save();
     res.status(200).json("Kullanıcı başarıyla oluşturuldu.");
   } catch (error) {
     res.status(400).json(error);
   }
 });
-
-
 
 
 module.exports = router;
