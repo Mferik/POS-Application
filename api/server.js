16109164e183e56436209a4d23b1c03f97e4bc30
Mferik <mfatiherikk@gmail.com>
2023-06-17 12:42:54 +0300

    express router kurulumu ve kategori post routeri eklendi

--- api/server.js
+++ api/server.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const app = express();
+const cors = require("cors");
 const port = 5000;
+
+//routes
+
+const categoryRoute = require("./routes/categories.js");
+
 dotenv.config();
 
 const connect = async () => {
@@ -14,6 +20,12 @@ const connect = async () => {
     console.log(error);
   }
 };
+
+//! middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/categories", categoryRoute);
 
 app.get("/", (req, res) => res.send("selam canım ben amcanım"));
 
