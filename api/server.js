ae83886594c7fd3073ad7053795d05bb69b1c7c2
Mferik <mfatiherikk@gmail.com>
2023-06-17 11:45:46 +0300

    mongodb kuruldu connection sağlandı

--- api/server.js
+++ api/server.js
@@ -1,10 +1,23 @@
-const express = require("express")
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
-
 const port = 5000;
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.get("/", (req, res) => res.send("selam canım ben amcanım"));
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`)
-})
+  connect();
+  console.log(`App is listening on port ${port}`);
+});
