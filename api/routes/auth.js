56382b28a870352713d2e290735c843b28822ff7
Mferik <mfatiherikk@gmail.com>
2023-06-17 14:01:54 +0300

    login validasyonu eklendi

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,7 @@ const User = require("../models/User.js");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-//! kullanıcı oluşturma
+//! register
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -20,5 +20,23 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//! login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send({ error: "Kullanıcı bulunamadı" });
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      res.status(403).send("Şifre doğru değil.");
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
