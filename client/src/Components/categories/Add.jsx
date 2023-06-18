018c026a5e9995b66ea683753e38736c6f56b845
Mferik <mfatiherikk@gmail.com>
2023-06-18 21:32:23 +0300

    category api cliente bağlandı

---
+++ client/src/Components/categories/Add.jsx
@@ -0,0 +1,56 @@
+import { Button, Form, Input, Modal, message } from "antd";
+import React from "react";
+
+const Add = ({
+  isAddModalOpen,
+  setIsAddModalOpen,
+  categories,
+  setCategories,
+}) => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    try {
+      fetch("http://localhost:5000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      message.success("Kategori başarıyla eklendi.");
+      form.resetFields();
+      setCategories([
+        ...categories,
+        {
+          _id: Math.random(),
+          title: values.title,
+        },
+      ]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Modal
+      title="Yeni Kategori Ekle"
+      open={isAddModalOpen}
+      onCancel={() => setIsAddModalOpen(false)}
+      footer={false}
+    >
+      <Form layout="vertical" onFinish={onFinish} form={form}>
+        <Form.Item
+          name="title"
+          label="Kategori Ekle"
+          rules={[{ required: true, message: "Kategori ismi zorunludur" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item className="flex justify-end mb-0">
+          <Button type="primary" htmlType="submit">
+            Ekle
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default Add;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
 import Header from "../Components/Header/Header";
 import CartTotals from "../Components/cart/CartTotals";
 import Categories from "../Components/categories/Categories";
 import Products from "../Components/products/Products";
 
 const HomePage = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/categories/get-all")
+        const data = await res.json();
+        
+        setCategories(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getCategories()
+  }, []);
+  
   return (
     <>
       <Header />
       <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
         <div className="categories max-h-[calc(100vh_-_112px)] overflow-y-auto  md:pb-10">
-          <Categories />
+          <Categories categories={categories} setCategories = {setCategories}/>
         </div>
         <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto ">
           <Products />
