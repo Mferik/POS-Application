f93b4bc7c15be4aeff33bc0a5f835bb0f5369651
Mferik <mfatiherikk@gmail.com>
2023-06-19 14:56:36 +0300

    sepete ekleme ve sayı güncelleme  işlemi redux toolkit ile

--- client/src/Components/products/ProductItem.jsx
+++ client/src/Components/products/ProductItem.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { addProduct } from "../../redux/cartSlice";
+import { useDispatch , useSelector} from "react-redux";
 
-const ProductItem = ({product}) => {
+const ProductItem = ({ product }) => {
+  const cart = useSelector((state) => state.cart)
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(addProduct(product))
+  }
+  console.log(cart.cartItems.length);
   return (
-    <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
+    <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none" onClick={handleClick}>
       <div className="product-img">
         <img
           src={product.img}
