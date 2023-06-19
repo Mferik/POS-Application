e568323ca95e37758c0994ab8e09ae41e8e2c835
Mferik <mfatiherikk@gmail.com>
2023-06-19 12:23:57 +0300

    dbden  ürünleri arayüze  getirme işlemi yapıldı

--- client/src/Components/products/Products.jsx
+++ client/src/Components/products/Products.jsx
@@ -1,189 +1,39 @@
+import { useEffect, useState } from "react";
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/products/get-all");
+        const data = await res.json();
+
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
+
   return (
     <div className="products-wrapper grid grid-cols-[repeat(auto-fill,_minmax(150px,_1fr))] gap-4">
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
+      {products.map((product) => (
+        <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
+          <div className="product-img">
+            <img
+              src={product.img}
+              alt=""
+              className="h-28 object-cover w-full border-b"
+            />
+          </div>
+          <div className="product-info flex flex-col p-3">
+            <span className="font-bold">{product.title}</span>
+            <span>{product.price}₺</span>
+          </div>
         </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-2xl cursor-pointer transition-all select-none">
-        <div className="product-img">
-          <img
-            src="https://productimages.hepsiburada.net/s/124/222-222/110000073784544.jpg/format:webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Erik</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      
+      ))}
     </div>
   );
 };
