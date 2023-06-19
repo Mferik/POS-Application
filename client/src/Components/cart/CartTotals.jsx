4b10a08933ad4304e9425478a498e8e6d7cc2231
Mferik <mfatiherikk@gmail.com>
2023-06-19 15:16:46 +0300

    sepetten ürün silme işlemi

--- client/src/Components/cart/CartTotals.jsx
+++ client/src/Components/cart/CartTotals.jsx
@@ -4,46 +4,45 @@ import {
   PlusCircleOutlined,
   MinusCircleOutlined,
 } from "@ant-design/icons";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteCart } from "../../redux/cartSlice";
 const CartTotals = () => {
-  const {cartItems} = useSelector((state) => state.cart)
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch()
   return (
     <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
       <h2 className="bg-blue-600 text-center px-4 py-4 text-white font-bold tracking-wide">
         Sepetteki Ürünler
       </h2>
       <ul className="cart-items px-2 flex flex-col gap-y-3 pt-2 py-2 overflow-y-auto ">
-       {cartItems.map((item) => (
-         <li className="cart-item flex justify-between" key={item._id}>
-         <div className="flex items-center">
-           <img
-             src={item.img}
-             alt=""
-             className="w-16 h-16 object-cover"
-           />
-           <div className="flex flex-col ml-2">
-             <b>{item.title}</b>
-             <span>{item.price}₺ x {item.quantity}</span>
-           </div>
-         </div>
-         <div className="flex items-center gap-x-2">
-           <Button
-             type="primary"
-             size="small"
-             className="w-full flex items-center justify-center !rounded-full "
-             icon={<PlusCircleOutlined />}
-           />
-           <span className="font-bold">{item.quantity}</span>
-           <Button
-             type="primary"
-             size="small"
-             className="w-full flex items-center justify-center !rounded-full "
-             icon={<MinusCircleOutlined />}
-           />
-         </div>
-       </li>
-       ))}
-        
+        {cartItems.map((item) => (
+          <li className="cart-item flex justify-between" key={item._id}>
+            <div className="flex items-center">
+              <img src={item.img} alt="" className="w-16 h-16 object-cover cursor-pointer" onClick={() => dispatch(deleteCart(item))}/>
+              <div className="flex flex-col ml-2">
+                <b>{item.title}</b>
+                <span>
+                  {item.price}₺ x {item.quantity}
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center gap-x-2">
+              <Button
+                type="primary"
+                size="small"
+                className="w-full flex items-center justify-center !rounded-full "
+                icon={<PlusCircleOutlined />}
+              />
+              <span className="font-bold">{item.quantity}</span>
+              <Button
+                type="primary"
+                size="small"
+                className="w-full flex items-center justify-center !rounded-full "
+                icon={<MinusCircleOutlined />}
+              />
+            </div>
+          </li>
+        ))}
       </ul>
       <div className="cart-totals mt-auto">
         <div className="border-b border-t">
