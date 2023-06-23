be55d2a55369e69fecff8da2f5082d7acb4c635b
Mferik <mfatiherikk@gmail.com>
2023-06-23 10:02:03 +0300

    api yolları dinamikleştirildi

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -11,7 +11,7 @@ const StatisticPage = () => {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await fetch("http://localhost:5000/api/products/get-all");
+        const res = await fetch(process.env.REACT_APP_SERVER_URL + "/api/products/get-all");
         const data = await res.json();
 
         setProducts(data);
@@ -27,7 +27,7 @@ const StatisticPage = () => {
   }, []);
   const asyncFetch = () => {
     fetch(
-      "http://localhost:5000/api/bills/get-all"
+      process.env.REACT_APP_SERVER_URL + "/api/bills/get-all"
     )
       .then((response) => response.json())
       .then((json) => setData(json))
